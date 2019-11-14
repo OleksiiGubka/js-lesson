503c71151f853ecf70b5ea2ac92239c900230fe3
Oleksii <oleksiigubka@gmail.com>
2019-11-14 14:21:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+
+//  var leftBorderWidth = 1;
+//  let second = 2;
+//  const pi = 3.14;
+// console.log(leftBorderWidth);
+
+// let arr = confirm("Вам есть 18?");
+
+// console.log(arr);
+// let arr = prompt("Вам есть 18?", "Да");
+
+// console.log(arr);
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(!isChecked || isClose);
